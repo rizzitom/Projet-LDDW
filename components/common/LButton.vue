fbe467ccef8325294f0d1da4f86f468f21b7737d
Théo Billardey <theo.billardey@gmail.com>
2021-06-10 21:43:36 +0200

    add LButton

---
+++ components/common/LButton.vue
@@ -0,0 +1,22 @@
+<template>
+  <button
+    :type="type"
+    class="bg-purple-600 text-white py-2 px-4 rounded-xl text-2xl"
+    @click="$emit('click')"
+  >
+    <slot />
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'LButton',
+
+  props: {
+    type: {
+      type: String,
+      default: 'button'
+    }
+  }
+}
+</script>
