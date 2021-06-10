ac653f92e34e4b65f06fae9d209a4889ca31071f
Théo Billardey <theo.billardey@gmail.com>
2021-06-10 22:53:36 +0200

    set bold font for header title

--- components/navigation/MainHeader.vue
+++ components/navigation/MainHeader.vue
@@ -5,7 +5,7 @@
     <div
       class="flex col-start-2 sm:col-start-2 sm:col-span-2 col-span-6 items-center"
     >
-      <span class="font-black">Le design du web</span>
+      <span class="font-bold">Le design du web</span>
     </div>
 
     <div class="col-span-2 col-end-12 items-center justify-end flex">
@@ -22,6 +22,7 @@
 <script>
 import MenuIcon from 'vue-material-design-icons/Menu.vue'
 import LButton from '../common/LButton.vue'
+
 export default {
   name: 'MainHeader',
   components: { LButton, MenuIcon }
