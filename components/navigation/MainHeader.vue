92bbb5963dd8b23b1d03feb986adbb295ae15b2e
Théo Billardey <theo.billardey@gmail.com>
2021-06-10 22:26:50 +0200

    add MainHeader

--- components/navigation/MainHeader.vue
+++ components/navigation/MainHeader.vue
@@ -3,27 +3,24 @@
     class="shadow-sm grid grid-cols-12 h-20 text-purple-600 auto-cols-auto"
   >
     <div
-      class="bg-blue-400 flex col-start-2 sm:col-start-2 sm:col-span-3 col-span-6 items-center"
+      class="flex col-start-2 sm:col-start-2 sm:col-span-2 col-span-6 items-center"
     >
       <span class="font-black">Le design du web</span>
     </div>
 
-    <div
-      class="bg-red-400 col-span-4 col-end-12 items-center justify-end hidden sm:flex"
-    >
-      <l-button>Devis gratuit</l-button>
-      <l-button class="ml-5">
+    <div class="col-span-2 col-end-12 items-center justify-end flex">
+      <l-button class="ml-5 hidden sm:block" anchor to="test">
         Se connecter
       </l-button>
+      <l-button class="sm:hidden">
+        <menu-icon />
+      </l-button>
     </div>
-    <l-button>
-      <menu-icon />
-    </l-button>
   </header>
 </template>
 
 <script>
-import { MenuIcon } from '@heroicons/vue/solid'
+import MenuIcon from 'vue-material-design-icons/Menu.vue'
 import LButton from '../common/LButton.vue'
 export default {
   name: 'MainHeader',
