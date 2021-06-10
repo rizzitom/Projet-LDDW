d527e9811a82abd7ea35b602e704949deb18c161
Théo Billardey <theo.billardey@gmail.com>
2021-06-10 22:32:57 +0200

    remove outline for LButton, add focus bg

--- components/common/LButton.vue
+++ components/common/LButton.vue
@@ -12,7 +12,7 @@
     <button
       v-else
       :type="type"
-      class="bg-purple-600 text-white py-2 px-4 rounded-xl text-2xl hover:bg-purple-700 duration-150"
+      class="bg-purple-600 text-white py-2 px-4 rounded-xl text-2xl hover:bg-purple-700 focus:bg-purple-700 focus:outline-none duration-150"
       @click="$emit('click')"
     >
       <slot />
