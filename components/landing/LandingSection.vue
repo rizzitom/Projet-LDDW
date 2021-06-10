58b700fe30ceb6c1f598f19e06f06a647f56515a
Théo Billardey <theo.billardey@gmail.com>
2021-06-10 23:18:16 +0200

    add LandingSection

---
+++ components/landing/LandingSection.vue
@@ -0,0 +1,13 @@
+<template>
+  <section class="grid grid-cols-12">
+    <div class="col-start-2 col-end-12">
+      <slot />
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'LandingSection'
+}
+</script>
