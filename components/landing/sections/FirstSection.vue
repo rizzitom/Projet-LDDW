f2d7df7724e866cbfdf312d84a77d4f19a515d40
Théo Billardey <theo.billardey@gmail.com>
2021-06-11 19:21:30 +0200

    add FirstSection

---
+++ components/landing/sections/FirstSection.vue
@@ -0,0 +1,37 @@
+<template>
+  <landing-section class="py-36" @scroll.native="handle">
+    <div class="flex">
+      <div class="flex-1">
+        <h1 class="text-8xl font-semibold">
+          Création de sites internet sur mesure
+        </h1>
+        <span class="w-9/12 flex my-10">
+          Du premier contact au livrable, notre équipe vous accompagne dans
+          chaque étape dans la création de votre projet. Tarifs flexibles et
+          révisions inclus.
+        </span>
+
+        <div class="flex">
+          <l-button class="mr-5">
+            Devis gratuit
+          </l-button>
+          <l-button variant>
+            Contact
+          </l-button>
+        </div>
+      </div>
+      <img src="/undraw_programmer.svg" alt="" class="block w-1/2">
+    </div>
+  </landing-section>
+</template>
+
+<script>
+import LandingSection from '../LandingSection.vue'
+import LButton from '../../common/LButton.vue'
+
+export default {
+  name: 'FirstSection',
+
+  components: { LandingSection, LButton }
+}
+</script>
