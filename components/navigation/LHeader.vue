d92b75dc01dd17ac6d8f04fd33cc8973963369b2
Théo Billardey <theo.billardey@gmail.com>
2021-06-12 19:08:48 +0200

    rename MainHeader in LHeader

--- components/navigation/LHeader.vue
+++ components/navigation/LHeader.vue
@@ -1,6 +1,6 @@
 <template>
   <header
-    class="fixed w-full bg-white grid grid-cols-12 h-20 text-purple-600 auto-cols-auto border border-gray-100"
+    class="fixed w-full bg-white grid grid-cols-12 h-20 text-purple-600 auto-cols-auto border border-gray-100 z-10"
   >
     <div
       class="flex col-start-2 sm:col-start-2 sm:col-span-2 col-span-6 items-center"
