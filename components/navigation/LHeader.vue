53bd7db97c70bb23b79ba1a7d06a78dbd4d357e7
Théo Billardey <theo.billardey@gmail.com>
2021-06-14 23:07:17 +0200

    set header title as a link

--- components/navigation/LHeader.vue
+++ components/navigation/LHeader.vue
@@ -2,13 +2,15 @@
   <header
     class="fixed w-full bg-white grid grid-cols-12 h-20 text-purple-600 auto-cols-auto border border-gray-100 z-10"
   >
-    <div
-      class="flex col-start-2 sm:col-start-2 col-span-6 sm:col-span-4 items-center"
-    >
-      <span class="font-bold">Le design du web</span>
+    <div class="col-start-2 sm:col-start-2 col-span-6 sm:col-span-4 font-bold flex items-center">
+      <NuxtLink to="/">
+        Le design du web
+      </NuxtLink>
     </div>
 
-    <div class="col-start-10 sm:col-start-8 col-end-12 items-center justify-end flex">
+    <div
+      class="col-start-10 sm:col-start-8 col-end-12 items-center justify-end flex"
+    >
       <l-button class="ml-5 hidden sm:block" anchor to="test">
         Se connecter
       </l-button>
