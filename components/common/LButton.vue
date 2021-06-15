c19e309ad4c5e287a30e86c71b1f12c2c0be97e5
Théo Billardey <theo.billardey@gmail.com>
2021-06-16 01:55:45 +0200

    add classes for LButton with anchor prop

--- components/common/LButton.vue
+++ components/common/LButton.vue
@@ -3,7 +3,28 @@
     <NuxtLink
       v-if="anchor"
       :to="to"
-      class="bg-purple-600 font-semibold text-white py-2 px-5 rounded-xl text-2xl hover:bg-purple-700 duration-150"
+      :class="[
+        {
+          'bg-purple-600': !variant,
+          'text-white': !variant,
+          'duration-150': !variant,
+          'py-2': !variant,
+          'px-5': !variant,
+          'rounded-xl': !variant,
+          'hover:bg-purple-700': !variant,
+          'focus:bg-purple-700': !variant,
+          'font-semibold': !variant,
+
+          'font-medium': variant,
+          'text-purple-600': variant,
+          'hover:text-purple-400': variant,
+          'focus:text-purple-400': variant,
+          'duration-150': variant
+        },
+        'align-middle',
+        'focus:outline-none',
+        'text-2xl'
+      ]"
       @click="$emit('click')"
     >
       <slot />
