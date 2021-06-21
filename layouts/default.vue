0ae498d2f6fd8bf49ef1cf272138353085051ec5
Théo Billardey <theo.billardey@gmail.com>
2021-06-21 22:34:28 +0200

    set default layout as blank page

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,18 +1,5 @@
 <template>
-  <div>
-    <l-header />
-    <main class="pt-20">
-      <nuxt />
-    </main>
-    <l-footer />
-  </div>
+  <main>
+    <nuxt />
+  </main>
 </template>
-
-<script>
-import LFooter from '../components/navigation/LFooter.vue'
-import LHeader from '../components/navigation/LHeader.vue'
-
-export default {
-  components: { LHeader, LFooter }
-}
-</script>
