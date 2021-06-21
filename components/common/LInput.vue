62e9088b4e0147f73a4a38b30898f2b6bf7932b1
Théo Billardey <theo.billardey@gmail.com>
2021-06-22 00:46:35 +0200

    add LInput

---
+++ components/common/LInput.vue
@@ -0,0 +1,19 @@
+<template>
+  <input
+    :type="type"
+    class="text-2xl py-3 px-5 rounded-xl border focus:border-purple-700 focus:outline-none transition duration-150"
+  >
+</template>
+
+<script>
+export default {
+  name: 'LInput',
+
+  props: {
+    type: {
+      type: String,
+      default: 'text'
+    }
+  }
+}
+</script>
