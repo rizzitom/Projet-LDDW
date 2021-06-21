5852bd4887f9c06095ea33c5e426884827161d1b
Théo Billardey <theo.billardey@gmail.com>
2021-06-22 01:11:49 +0200

    fix v-model for LInput

--- components/common/LInput.vue
+++ components/common/LInput.vue
@@ -1,7 +1,9 @@
 <template>
   <input
     :type="type"
+    :value="value"
     class="text-2xl py-3 px-5 rounded-xl border focus:border-purple-700 focus:outline-none transition duration-150"
+    @change="$emit('input', $event.target.value)"
   >
 </template>
 
@@ -13,6 +15,10 @@ export default {
     type: {
       type: String,
       default: 'text'
+    },
+    value: {
+      type: String,
+      default: ''
     }
   }
 }
