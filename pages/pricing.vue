67cd0fe7c4fe668bf213c268f90ae5a26f15a07f
Théo Billardey <theo.billardey@gmail.com>
2021-06-23 21:46:24 +0200

    set grid layout for pricing page

--- pages/pricing.vue
+++ pages/pricing.vue
@@ -1,45 +1,44 @@
 <template>
-  <div class="py-36">
-    <landing-section>
-      <div class="text-center mb-24">
+  <div class="py-36 grid grid-cols-12">
+    <div class="col-span-10 col-start-2 lg:col-start-4 lg:col-span-6 grid grid-cols-2 gap-x-12 gap-y-24 md:gap-y-32 items-center">
+      <div class="text-center col-span-2">
         <h1 class="text-6xl font-semibold mb-4">
           Nos formules et tarifs
         </h1>
-        <span class="text-h3 md:w-1/2 block m-auto">
+        <span class="text-h3">
           Des tarifs abordables et des formules flexibles pour donner vie à
           n'importe quel type de projet.
         </span>
       </div>
 
-      <div class="flex flex-col sm:flex-row sm:justify-center sm:items-end">
-        <l-pricing-card
-          class="w-full sm:w-1/2 lg:w-1/3 xl:w-1/4 sm:mr-8 mb-12 sm:mb-0"
-          name="Standard"
-          :price="40"
-          subheading="par jour"
-          :features="[
-            '5 révisions gratuites',
-            'Support disponible 7j/7',
-            'Suivi de commande en temps réel',
-            'Accessibilité et performance inclus'
-          ]"
-          warn="5 jours de travail minimum pour sélectionner cette formule"
-        />
-        <l-pricing-card
-          class="w-full sm:w-1/2 lg:w-1/3 xl:w-1/4"
-          name="Flexible"
-          :price="7"
-          subheading="par heure"
-          :features="[
-            '5 révisions gratuites',
-            'Support disponible 7j/7',
-            'Suivi de commande en temps réel',
-            'Accessibilité et performance inclus'
-          ]"
-        />
-      </div>
+      <l-pricing-card
+        class="col-span-2 sm:col-span-1"
+        name="Standard"
+        :price="40"
+        subheading="par jour"
+        :features="[
+          '5 révisions gratuites',
+          'Support disponible 7j/7',
+          'Suivi de commande en temps réel',
+          'Accessibilité et performance inclus'
+        ]"
+        warn="5 jours de travail minimum pour sélectionner cette formule"
+      />
+      <l-pricing-card
+        class="col-span-2 sm:col-span-1"
+        name="Flexible"
+        :price="7"
+        subheading="par heure"
+        :features="[
+          '5 révisions gratuites',
+          'Support disponible 7j/7',
+          'Suivi de commande en temps réel',
+          'Accessibilité et performance inclus'
+        ]"
+      />
+
       <div
-        class="w-full mt-24 rounded-2xl shadow-lg p-8 bg-purple-600 text-white"
+        class="col-span-2 rounded-2xl shadow-lg p-8 bg-purple-600 text-white"
       >
         <h3 class="font-medium text-3xl mb-3">
           Un doute, une question ?
@@ -51,16 +50,15 @@
           pour avoir des informations supplémentaires sur la tarification
         </p>
       </div>
-    </landing-section>
+    </div>
   </div>
 </template>
 
 <script>
-import LandingSection from '../components/landing/LandingSection.vue'
 import LPricingCard from '../components/pricing/LPricingCard.vue'
 
 export default {
-  components: { LandingSection, LPricingCard },
+  components: { LPricingCard },
 
   head () {
     return {
