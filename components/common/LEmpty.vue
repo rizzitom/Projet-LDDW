f110343eadfc0d1ff4720354a129c40ccd144177
Théo Billardey <theo.billardey@gmail.com>
2021-07-02 23:28:36 +0200

    add LEmpty

---
+++ components/common/LEmpty.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="text-2xl flex flex-col items-center">
+    <span class="material-icons icon">
+      air
+    </span>
+    <span class="mt-4">{{ title }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LEmpty',
+
+  props: {
+    title: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+.icon {
+  font-size: 52px;
+}
+</style>
