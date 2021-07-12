abe34278820435b8931cba30c422d42178a6ae79
Théo Billardey <theo.billardey@gmail.com>
2021-07-13 01:23:24 +0200

    add paid invoices function

---
+++ functions/index.js
@@ -0,0 +1,30 @@
+/* eslint-disable indent */
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+admin.initializeApp();
+const db = admin.firestore();
+
+exports.updateStripInvoice = functions.firestore
+  .document("invoices/{stripeInvoiceId}")
+  .onUpdate((change) => {
+    const updatedInvoice = change.after.data();
+
+    if (
+      updatedInvoice.orderId &&
+      updatedInvoice.stripeInvoiceStatus === "paid"
+    ) {
+      // update order step when the order is paid
+      /* console.log("order paid, updating order step..."); */
+      const orderId = updatedInvoice.orderId;
+      return db.doc(`orders/${orderId}`).set({
+        step: 3,
+      });
+      /* .then(() => {
+          console.log(`order ${orderId} set to step 3`);
+        }); */
+    } else if (!updatedInvoice.orderId) {
+      return console.error("Cannot update order: no order id in invoice");
+    } else {
+      return null;
+    }
+  });
