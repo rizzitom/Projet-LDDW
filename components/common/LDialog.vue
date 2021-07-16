c66815d1caa1c87773dfef4481105a5904819ed6
Théo Billardey <theo.billardey@gmail.com>
2021-07-16 23:40:59 +0200

    add LDialog

---
+++ components/common/LDialog.vue
@@ -0,0 +1,44 @@
+<template>
+  <div
+    class="p-12 grid grid-cols-12 top-0 left-0 fixed w-full h-full bg-gray-800 bg-opacity-50 z-10 cursor-default items-center"
+    @click="handleClose"
+  >
+    <div
+      class="bg-white rounded-xl col-start-1 col-span-12 sm:col-start-3 sm:col-span-8 lg:col-start-5 lg:col-span-4 2xl:col-start-6 2xl:col-span-3 max-h-full overflow-y-scroll"
+      @click.stop=""
+    >
+      <div
+        class="py-4 px-6 flex justify-between items-center bg-purple-600 text-white rounded-t-xl"
+      >
+        <span class="text-2xl flex flex-1">{{ title }}</span>
+        <l-button variant class="ml-12 md:ml-24" @click="handleClose">
+          <span class="text-white">Fermer</span>
+        </l-button>
+      </div>
+
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script>
+import LButton from '../common/LButton.vue'
+
+export default {
+  name: 'LDialog',
+  components: { LButton },
+
+  props: {
+    title: {
+      type: String,
+      required: true
+    }
+  },
+
+  methods: {
+    handleClose (e) {
+      this.$emit('close', e)
+    }
+  }
+}
+</script>
