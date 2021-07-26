39c6cc915097093f95fdff2fc6b72342b1b46c27
Théo Billardey <theo.billardey@gmail.com>
2021-07-26 22:28:29 +0200

    add ellipsis overflow for dialog title

--- components/common/LDialog.vue
+++ components/common/LDialog.vue
@@ -10,7 +10,7 @@
       <div
         class="py-4 px-6 flex justify-between items-center bg-purple-600 text-white rounded-t-xl"
       >
-        <span class="text-2xl flex flex-1">{{ title }}</span>
+        <span class="text-2xl flex-1 truncate inline-block">{{ title }}</span>
         <l-button variant class="ml-12 md:ml-24" @click="handleClose">
           <span class="text-white">Fermer</span>
         </l-button>
