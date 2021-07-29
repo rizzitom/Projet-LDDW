c9794ff0b48d33a86b50b53b92582216e1710547
Théo Billardey <theo.billardey@gmail.com>
2021-07-30 00:23:01 +0200

    add loading state for LChip

--- components/common/LChip.vue
+++ components/common/LChip.vue
@@ -1,11 +1,35 @@
 <template>
   <div
-    class="rounded-full py-1 px-3 flex items-center text-lg text-white"
+    class="rounded-full py-1 px-3 flex items-center text-lg text-white h-full"
     :class="[`bg-${color}`]"
   >
-    <span v-if="icon" class="material-icons mr-1 chip-icon"> {{ icon }} </span>
+    <svg
+      v-if="loading"
+      class="animate-spin h-5 w-5 text-white"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+    >
+      <circle
+        class="opacity-25"
+        cx="12"
+        cy="12"
+        r="10"
+        stroke="currentColor"
+        stroke-width="4"
+      />
+      <path
+        class="opacity-75"
+        fill="currentColor"
+        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+      />
+    </svg>
 
-    <span>{{ title }}</span>
+    <span v-if="icon && !loading" class="material-icons mr-1 chip-icon">
+      {{ icon }}
+    </span>
+
+    <span v-if="!loading">{{ title }}</span>
   </div>
 </template>
 
@@ -25,6 +49,10 @@ export default {
     color: {
       type: String,
       default: 'gray-500'
+    },
+    loading: {
+      type: Boolean,
+      default: false
     }
   }
 }
