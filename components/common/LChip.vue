cae1513a1e795980969093cc57c175fa8669de3b
Théo Billardey <theo.billardey@gmail.com>
2021-07-29 23:56:25 +0200

    add LChip

---
+++ components/common/LChip.vue
@@ -0,0 +1,37 @@
+<template>
+  <div
+    class="rounded-full py-1 px-3 flex items-center text-lg text-white"
+    :class="[`bg-${color}`]"
+  >
+    <span v-if="icon" class="material-icons mr-1 chip-icon"> {{ icon }} </span>
+
+    <span>{{ title }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LChip',
+
+  props: {
+    title: {
+      type: String,
+      default: ''
+    },
+    icon: {
+      type: String,
+      default: ''
+    },
+    color: {
+      type: String,
+      default: 'gray-500'
+    }
+  }
+}
+</script>
+
+<style scoped>
+.chip-icon {
+  font-size: 16px;
+}
+</style>
