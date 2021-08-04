3ec19be50870e9fd55b368ba00e93fac7476e6ea
Théo Billardey <theo.billardey@gmail.com>
2021-08-05 00:01:34 +0200

    add LSelect

---
+++ components/common/LSelect.vue
@@ -0,0 +1,30 @@
+<template>
+  <select
+    :type="type"
+    :value="value"
+    class="text-2xl py-3 px-5 rounded-xl border focus:border-purple-700 focus:shadow-input focus:outline-none transition duration-150"
+    @change="$emit('input', $event.target.value)"
+  >
+    <option selected disabled hidden value="">
+      {{ placeholder }}
+    </option>
+    <slot />
+  </select>
+</template>
+
+<script>
+export default {
+  name: 'LSelect',
+
+  props: {
+    value: {
+      type: [String, Number],
+      default: undefined
+    },
+    placeholder: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
