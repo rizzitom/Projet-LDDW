c5475679ba3c7a09509544d7e039b492284ac2e6
Théo Billardey <theo.billardey@gmail.com>
2021-08-06 01:53:50 +0200

    add contact illustration

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,8 @@ module.exports = {
         'step-three-illustration': "url('/undraw_codereview.svg')",
         'step-four-illustration': "url('/undraw_projectcompleted.svg')",
         'not-found-illustration': "url('/undraw_empty.svg')",
-        'server-error-illustration': "url('/undraw_warning.svg')"
+        'server-error-illustration': "url('/undraw_warning.svg')",
+        'contact-illustration': "url('/undraw_mailbox.svg')"
       })
     },
     boxShadow: {
