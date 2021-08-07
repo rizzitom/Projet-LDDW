1796e118b8d69dc2f754946f1f34a2cdb5587d19
Théo Billardey <theo.billardey@gmail.com>
2021-08-07 14:15:16 +0200

    fix shadow config

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,10 @@ module.exports = {
       })
     },
     boxShadow: {
-      input: '0 0 0 3px rgba(125, 72, 238, 0.4)'
+      input: '0 0 0 3px rgba(125, 72, 238, 0.4)',
+      DEFAULT: '0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06)',
+      sm: '0 1px 2px 0 rgba(0, 0, 0, 0.05)',
+      lg: '0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)'
     }
   },
   variants: {
